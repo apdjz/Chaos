81f37ec6edccf043498aa21bde70ecc0447c88c4
Andrew Ziegler <14apdjz@gmail.com>
2023-03-21 17:58:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,79 +1,44 @@
-let word = "Chaos";
-//returns array
-arr = word.split("");
-let first = arr.shift();
-// let reversed = arr.reverse().join("");
-let reversed = arr.join("");
-
-function flipString(aString) {	
-  return String(aString)
-    .split("")
-    .map(c => flipTable[c] || c)
-    .reverse()
-    .join("")
+function flipAndReverse(str) {
+  const charMap = {
+    "a": "\u0250",
+    "b": "q",
+    "c": "\u0254",
+    "d": "p",
+    "e": "\u01DD",
+    "f": "\u025F",
+    "g": "\u0183",
+    "h": "\u0265",
+    "i": "\u0131",
+    "j": "\u027E",
+    "k": "\u029E",
+    "l": "\u05DF",
+    "m": "\u026F",
+    "n": "u",
+    "o": "o",
+    "p": "d",
+    "q": "b",
+    "r": "\u0279",
+    "s": "s",
+    "t": "\u0287",
+    "u": "n",
+    "v": "\u028C",
+    "w": "\u028D",
+    "x": "x",
+    "y": "\u028E",
+    "z": "z",
+    " ": " "
+  };
+  
+  let flippedChars = "";
+  
+  for (let i = str.length - 1; i >= 0; i--) {
+    const char = str.charAt(i);
+    const flippedChar = charMap[char] || char;
+    flippedChars += flippedChar;
+  }
+  
+  return flippedChars;
 }
+const flippedString = flipAndReverse("Chaos");
+console.log(flippedString);
 
-var flipTable = {
-'\u0021' : '\u00A1',
-'\u0022' : '\u201E',
-'\u0026' : '\u214B',
-'\u0027' : '\u002C',
-'\u0028' : '\u0029',
-'\u002E' : '\u02D9',
-'\u0033' : '\u0190',
-'\u0034' : '\u152D',
-'\u0036' : '\u0039',
-'\u0037' : '\u2C62',
-'\u003B' : '\u061B',
-'\u003C' : '\u003E',
-'\u003F' : '\u00BF',
-'\u0041' : '\u2200',
-'\u0042' : '\u10412',
-'\u0043' : '\u2183',
-'\u0044' : '\u25D6',
-'\u0045' : '\u018E',
-'\u0046' : '\u2132',
-'\u0047' : '\u2141',
-'\u004A' : '\u017F',
-'\u004B' : '\u22CA',
-'\u004C' : '\u2142',
-'\u004D' : '\u0057',
-'\u004E' : '\u1D0E',
-'\u0050' : '\u0500',
-'\u0051' : '\u038C',
-'\u0052' : '\u1D1A',
-'\u0054' : '\u22A5',
-'\u0055' : '\u2229',
-'\u0056' : '\u1D27',
-'\u0059' : '\u2144',
-'\u005B' : '\u005D',
-'\u005F' : '\u203E',
-'\u0061' : '\u0250',
-'\u0062' : '\u0071',
-'\u0063' : '\u0254',
-'\u0064' : '\u0070',
-'\u0065' : '\u01DD',
-'\u0066' : '\u025F',
-'\u0067' : '\u0183',
-'\u0068' : '\u0265',
-'\u0069' : '\u0131',
-'\u006A' : '\u027E',
-'\u006B' : '\u029E',
-'\u006C' : '\u0283',
-'\u006D' : '\u026F',
-'\u006E' : '\u0075',
-'\u0072' : '\u0279',
-'\u0074' : '\u0287',
-'\u0076' : '\u028C',
-'\u0077' : '\u028D',
-'\u0079' : '\u028E',
-'\u007B' : '\u007D',
-'\u203F' : '\u2040',
-'\u2045' : '\u2046',
-'\u2234' : '\u2235'
-}
-
-Object.keys(flipTable).forEach(i => flipTable[flipTable[i]] = i)
-
-let flipped = flipString(reversed);
-console.log(first + flipped)
